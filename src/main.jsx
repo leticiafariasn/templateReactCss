f09d719bbfc24b2b99ef49aaaa43e4f48951f6b1
Letícia Farias <lf182000@gmail.com>
2025-05-15 00:51:36 -0300

    revert

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./styles/index.css"
+import React from "react";
+import reactDOM from "react-dom/client"
+import "./App.css"
+import App from "./App.jsx"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = reactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
