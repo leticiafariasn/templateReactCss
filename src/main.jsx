1cfa8840d5b8480244dcc1587286ec278e5ba48b
Letícia Farias <lf182000@gmail.com>
2025-05-15 00:50:39 -0300

    fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import reactDOM from "react-dom/client"
-import "./App.css"
-import App from "./App.jsx"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./styles/index.css"
 
-const root = reactDOM.createRoot(document.getElementById("root"))
-
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
